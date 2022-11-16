7c6cf5abf2aeb581c9906cd3a8dfb3f1e1adaf75
N. Paila <n.paila01@ufromail.cl>
2022-11-16 11:47:31 -0300

    agrega menú de selección de Juego

--- src/main/java/Casino.java
+++ src/main/java/Casino.java
@@ -1,9 +1,8 @@
 public class Casino {
 
     public void iniciar() {
-        JuegoDeCarta juego = Carioca.crearNuevoJuego();
 
-        System.out.print("¿Que desea jugar?\n> ");
+        JuegoDeCarta juego = pedirJuego();
 
         Jugador dumbo = Jugador.crearJugador();
         dumbo.agregarAMonto(500);
@@ -18,14 +17,25 @@ public class Casino {
         juego.jugar();
     }
 
-
-    private void jugar(JuegoDeCarta juego) {
-
-        if (juego instanceof Blackjack) {
-
+    private JuegoDeCarta pedirJuego() {
+        mostrarJuegos();
+        int opcion = Utils.pedirOpcionHasta(3);
+        switch (opcion) {
+            case 0 -> System.exit(0);
+            case 1 -> {return Blackjack.crearNuevoJuego();}
+            case 2 -> {return Carioca.crearNuevoJuego();}
+            case 3 -> {return Escoba.crearNuevoJuego();}
         }
-
+        throw new IllegalStateException();
     }
 
+    private void mostrarJuegos() {
+        System.out.println("¿Que desea jugar?");
+        System.out.println("[1] Blackjack");
+        System.out.println("[2] Carioca");
+        System.out.println("[3] Escoba");
+        System.out.println("Para salir escriba [0]");
+        System.out.print("> ");
+    }
 }
 
